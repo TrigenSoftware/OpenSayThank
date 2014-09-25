b2c661859723bd2eb2d3e2cd60b5b2ad8cec46f0
Dan Onoshko <d.onoshko.student@sibneuro.ru>
2014-09-26 00:55:41 +0700

    fix

--- web/app.php
+++ web/app.php
@@ -14,16 +14,28 @@ $apcLoader = new ApcClassLoader('sf2', $loader);
 $loader->unregister();
 $apcLoader->register(true);
 */
+// Debug::enable();
+// require_once __DIR__.'/../app/AppKernel.php';
+// //require_once __DIR__.'/../app/AppCache.php';
+
+// $kernel = new AppKernel('prod', true);
+// $kernel->loadClassCache();
+// //$kernel = new AppCache($kernel);
+
+// // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
+// //Request::enableHttpMethodParameterOverride();
+// $request = Request::createFromGlobals();
+// $response = $kernel->handle($request);
+// $response->send();
+// $kernel->terminate($request, $response);
+
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 Debug::enable();
+
 require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
 
-$kernel = new AppKernel('prod', true);
+$kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
-
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
