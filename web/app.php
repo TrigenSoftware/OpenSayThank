2b092145af871cda03d6b605bff16099ae3eda1a
Dan Onoshko <d.onoshko.student@sibneuro.ru>
2014-09-26 00:22:29 +0700

    fix

--- web/app.php
+++ web/app.php
@@ -16,14 +16,18 @@ $apcLoader->register(true);
 
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
-
+error_reporting(E_ALL | E_STRICT);
 $kernel = new AppKernel('prod', true);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
-
+error_reporting(E_ALL | E_STRICT);
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
+error_reporting(E_ALL | E_STRICT);
 $response = $kernel->handle($request);
+error_reporting(E_ALL | E_STRICT);
 $response->send();
+error_reporting(E_ALL | E_STRICT);
 $kernel->terminate($request, $response);
+error_reporting(E_ALL | E_STRICT);
