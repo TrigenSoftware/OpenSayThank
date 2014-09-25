1c585109dd654486e2284b9e2417ceda912d8b08
Dan Onoshko <d.onoshko.student@sibneuro.ru>
2014-09-26 00:37:15 +0700

    fix

--- web/app.php
+++ web/app.php
@@ -2,6 +2,7 @@
 
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
@@ -13,21 +14,17 @@ $apcLoader = new ApcClassLoader('sf2', $loader);
 $loader->unregister();
 $apcLoader->register(true);
 */
-
+Debug::enable();
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
-error_reporting(E_ALL | E_STRICT);
+
 $kernel = new AppKernel('prod', true);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
-error_reporting(E_ALL | E_STRICT);
+
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
-error_reporting(E_ALL | E_STRICT);
 $response = $kernel->handle($request);
-error_reporting(E_ALL | E_STRICT);
 $response->send();
-error_reporting(E_ALL | E_STRICT);
 $kernel->terminate($request, $response);
-error_reporting(E_ALL | E_STRICT);
